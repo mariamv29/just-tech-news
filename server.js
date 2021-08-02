56960f388f81633746443953b0ec22bab05cdab4
Maria Mireles <mireles335@gmail.com>
2021-08-02 15:09:55 -0500

    css not working

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
-const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
