d615a26cb2c12b09473aebf1ec2784745a755542
Maria Mireles <mireles335@gmail.com>
2021-08-02 23:03:49 -0500

    adding template engine

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+
+//handlebars template engine 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
